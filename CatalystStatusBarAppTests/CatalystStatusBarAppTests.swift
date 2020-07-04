1bdb7ea23d653a3a680c947f15e72452b61d6a28
Takahiko Inayama <sagittariusm25@gmail.com>
2020-07-04 21:48:35 +0900

    Initial Commit

---
+++ CatalystStatusBarAppTests/CatalystStatusBarAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  CatalystStatusBarAppTests.swift
+//  CatalystStatusBarAppTests
+//
+//  Created by Takahiko Inayama on 2020/07/04.
+//  Copyright © 2020 TETRA2000. All rights reserved.
+//
+
+import XCTest
+@testable import CatalystStatusBarApp
+
+class CatalystStatusBarAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
